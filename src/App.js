59352a516417b68a7c7539115c559d452afbe2fb
ocsfwarch <webmaster@ocsfwarch.com>
2019-10-16 15:44:22 -0700

    Initial Project Creation

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>OCSA Standard App Template</h1>
+   </div>
+  );
+}
+
+export default App;
